e03ccdf6304f4eb3cb952eb87c128df6ac606173
Syudon <129071327+Syudon@users.noreply.github.com>
2024-09-11 11:47:34 +0200

    add prinft message + print scanf float

---
+++ makeaFloat.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {   
+    float number = 9 ;
+   
+   
+    printf("Enter a float: ");  
+    scanf("%f",&number);
+    // displays output
+    printf("You entered: %f", number);
+    
+    return 0;
+}
