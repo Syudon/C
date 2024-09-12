a77ff411fe6edcc14a4f0158e4e0d0daa3a18361
Syudon <129071327+Syudon@users.noreply.github.com>
2024-09-12 11:40:09 +0200

    Create OuvertureFichier.c

---
+++ OuvertureFichier.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void main()
+
+
+{
+    float x,y;
+    int i =0;
+
+    filt*fchier=NULL;
+
+    fichier = fopen("data.txt","r");
+
+    if(fichier!=NULL)
+    {
+        while (fconf(fichier,"%f\t%f",&x,&y)!=)
+        {tabX[i]=x;tabY[i]=y;i++}
+
+
+        fclose(fichier);
+    }
+}
+
+/*
+
+int getc(FILE *)
+int putc(char, FILE *)
+
+*/
+
